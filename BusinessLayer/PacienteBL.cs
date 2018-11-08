f60b0b9906944c05ee85b0ac5fd54c197827e8e1
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-11-08 10:24:10 -0500

    VersionBase
    Se desarrolló la primera parte con el docente.

---
+++ BusinessLayer/PacienteBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using AccesoDatos;
+
+namespace BusinessLayer
+{
+    public class PacienteBL
+    {
+        public static List<Paciente> Listpacientes;
+
+        public List<Paciente> GetPacientes()
+        {
+            string path = PacientesDAL.ConexionPacientes();
+            string[] lines = System.IO.File.ReadAllLines($"{path}\\BD\\Pacientes.txt");
+            Listpacientes = new List<Paciente>();
+            foreach (var item in lines)
+            {
+                string Dni = item.Split(',')[0];
+                string Nombre = item.Split(',')[1];
+                string Apellido = item.Split(',')[2];
+                Paciente p = new Paciente(Dni, Nombre, Apellido);
+                Listpacientes.Add(p);
+            }
+            return Listpacientes;
+        }
+    }
+}
---
+++ DataAccess/PacientesDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace AccesoDatos
+{
+    public class PacientesDAL
+    {
+        public static string ConexionPacientes()
+        {
+            string pathBDPacientes = System.IO.Path.GetDirectoryName(
+                Assembly.GetEntryAssembly().Location);
+            return pathBDPacientes;
+        }
+    }
+}
+    
