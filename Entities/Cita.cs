d7186fa4e21080c771b4d896dd133ccae891b2e1
Eliezer J Quispe Cabana <elie.qc95@gmail.com>
2018-11-08 12:57:23 -0500

    Clase Cita.cs creada
    Entities>Cita.cs

---
+++ Entities/Cita.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    class Cita
+    {
+        public string COD_Cita { get; set; }
+        public string DNI_Paciente { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string COD_Especialidad { get; set; }
+        public string COD_Doctor { get; set; }
+        public string TipoCita { get; set; }
+        public string Estado { get; set; }
+
+        public Cita(string COD_Cita, string DNI_Paciente, string Nombres, string Apellidos, string COD_Especialidad, string COD_Doctor, string TipoCita , string Estado = "PENDIENTE")
+        {
+            this.COD_Cita = COD_Cita;
+            this.DNI_Paciente = DNI_Paciente;
+            this.Nombres = Nombres;
+            this.Apellidos = Apellidos;
+            this.COD_Especialidad = COD_Especialidad;
+            this.COD_Doctor = COD_Doctor;
+            this.TipoCita = TipoCita;
+            this.Estado = Estado;
+        }
+    }
+}
